afcd9d40ec261d26a7bd055d10a110daea2f436f
Qudus Tosin Aileru <46765590+Tosin5S@users.noreply.github.com>
2023-11-09 21:28:15 +0100

    Algorithm to find the maximum and minimum elements in a list of n data items:

---
+++ pq/2022/q4a.java
@@ -0,0 +1,11 @@
+/*
+*Input: A list of n data items, L
+*Output: The maximum and minimum elements in L
+
+*1. Initialize two variables, max and min, with the first element of L.
+*2. For i = 2 to n:
+    * If L[i] > max, then set max = L[i].
+    * If L[i] < min, then set min = L[i].
+*3. Return max and min.
+*/
+

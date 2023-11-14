6b0be62646e5a770cc932c1537ff1190c77e17d9
unknown <olyguy.aq@gmail.com>
2023-11-14 22:36:52 +0100

    A two dimensional array is actually an array of arrays. Each row is a separate array

---
+++ Practice/Java Arrays and Vectors/Test.java
@@ -0,0 +1,7 @@
+class Test {
+    public static void main (String args[]) {
+        int [][] A = new int[7][9];
+        System.out.println("A.length = " + A.length);
+        System.out.println("A[0].length = " + A[0].length);
+    }
+}

dd8bdf1ae24abde635845ebc7fdcfb5e036603da
unknown <olyguy.aq@gmail.com>
2023-12-02 09:10:26 +0100

    Length of string

---
+++ Practice/Java String Manipulations/Length.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Length {
+    public static void main (String [] args) {
+        String str = "I am now a student";
+        int strLength = str.length();
+        System.out.println("Length of the string given is " + strLength);
+    }
+}

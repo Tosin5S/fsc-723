368ef46e2f731a35faa1d50deb804671c827616e
unknown <olyguy.aq@gmail.com>
2023-12-16 20:39:45 +0100

    A regular expression for vehicle plate numbers in Nigeria

---
+++ Practice/Java String Manipulations/RegularExp.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class RegularExp {
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("This program demonstrates regular expressions in Java");
+
+        // Data Entry
+        System.out.println("Enter your name, surname, first and then others");
+        String name = input.nextLine();
+        System.out.println("Enter your phone number");
+        String phone = input.nextLine();
+        System.out.println("Enter your office address in the form: No. 4, Adeyi Street, Agbowo Ibadan.");
+        String officeAdd = input.nextLine();
+
+        // Regular Expression Testing 
+        if ()
+    }
+}

157070559d9a21b5054af686a01294e6725b6a60
unknown <olyguy.aq@gmail.com>
2023-12-20 21:39:41 +0100

    continuation of a regular expression for Vehicle Plate Numbers in Nigeria

--- Practice/Java String Manipulations/RegularExp.java
+++ Practice/Java String Manipulations/RegularExp.java
@@ -13,6 +13,23 @@ public class RegularExp {
         String officeAdd = input.nextLine();
 
         // Regular Expression Testing 
-        if ()
+        if (!name.matches("[A-Z][a-zA-Z]*\\s[a-zA-Z]*")) {
+            System.out.println("This is not a valid name");
+        }
+        else {
+            System.out.println("This is a valid name");
+            if (!phone.matches("[0]\\d{10}")) {
+                System.out.println("This is not a valid phone number");
+            }
+            else {
+                System.out.println("This is a valid phone number");
+            }
+
+            if (!officeAdd.matches ("[N][o].\\s\\d{1,},\\s\\w{2,}\\s\\w{2,},\\s\\w{2,}\\s\\w{2,}."))
+                System.out.println("This is not a valid Office Address");
+            else 
+                System.out.println("This is a valid office Address");
+        }
+
     }
 }

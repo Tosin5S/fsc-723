85b02589fde46a7002513eb5dc141324e9166d45
unknown <olyguy.aq@gmail.com>
2024-01-10 22:03:45 +0100

    nested loop for array

---
+++ Tests/q1c.java
@@ -0,0 +1,9 @@
+class q1c { 
+    public static void main (String [] args) {
+        for (int i = 0; i < r.length; i++) {
+            for (int j = 0; j < c; i++) {
+                
+            }
+        }
+    }
+}
